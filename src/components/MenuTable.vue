d8c9db9ba558af1bcdcb606e930abaeee9d23ca5
Peter Reginald <peter@digitalsanctum.com>
2020-12-22 09:29:27 +1100

    add function to edit name

--- src/components/MenuTable.vue
+++ src/components/MenuTable.vue
@@ -15,11 +15,37 @@
       :headers="headers"
       :items="menu"
       :search="search"
-      item-key="name"
-      show-select
-      class="elevation-1"
+      item-key="name+caegory"
     >
+      show-select class="elevation-1" >
+      <template v-slot:item.name="props">
+        <v-edit-dialog
+          :return-value.sync="props.item.name"
+          @save="save"
+          @cancel="cancel"
+          @open="open"
+          @close="close"
+        >
+          {{ props.item.name }}
+          <template v-slot:input>
+            <v-text-field
+              v-model="props.item.name"
+              :rules="[max25chars]"
+              label="Edit"
+              single-line
+              counter
+            ></v-text-field>
+          </template>
+        </v-edit-dialog>
+      </template>
     </v-data-table>
+    <v-snackbar v-model="snack" :timeout="3000" :color="snackColor">
+      {{ snackText }}
+
+      <template v-slot:action="{ attrs }">
+        <v-btn v-bind="attrs" text @click="snack = false"> Close </v-btn>
+      </template>
+    </v-snackbar>
     <v-card-actions>
       <v-btn @click="process">Go!</v-btn>
     </v-card-actions>
@@ -31,6 +57,12 @@ import { mapState } from "vuex";
 
 export default {
   data: () => ({
+    snack: false,
+    snackColor: "",
+    snackText: "",
+    max25chars: (v) => v.length <= 25 || "Input too long!",
+    pagination: {},
+
     selected: [],
     search: "",
     headers: [
@@ -43,6 +75,24 @@ export default {
   methods: {
     process() {
       console.log(this.selected);
+    },
+    save() {
+      this.snack = true;
+      this.snackColor = "success";
+      this.snackText = "Data saved";
+    },
+    cancel() {
+      this.snack = true;
+      this.snackColor = "error";
+      this.snackText = "Canceled";
+    },
+    open() {
+      this.snack = true;
+      this.snackColor = "info";
+      this.snackText = "Dialog opened";
+    },
+    close() {
+      console.log("Dialog closed");
     },
   },
 };
