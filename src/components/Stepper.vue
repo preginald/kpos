e71d16f36e5a2244b0c54eb12c24ca64b958e449
Peter Reginald <peter@digitalsanctum.com>
2020-12-22 17:32:05 +1100

    add Step 2

--- src/components/Stepper.vue
+++ src/components/Stepper.vue
@@ -18,7 +18,7 @@
     </v-stepper-header>
     <v-stepper-items>
       <v-stepper-content step="1"> <Step1 /> </v-stepper-content>
-      <v-stepper-content step="2"> test 2 </v-stepper-content>
+      <v-stepper-content step="2"> <Step2 /> </v-stepper-content>
       <v-stepper-content step="3"> test 3 </v-stepper-content>
     </v-stepper-items>
   </v-stepper>
@@ -28,12 +28,13 @@
 import { mapState } from "vuex";
 
 import Step1 from "@/components/Stepper/Step1.vue";
+import Step2 from "@/components/Stepper/Step2.vue";
 
 export default {
   data: () => ({
     stepper: 1,
   }),
-  components: { Step1 },
+  components: { Step1, Step2 },
   computed: mapState(["steps"]),
   methods: {},
 };
